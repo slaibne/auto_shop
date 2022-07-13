a432b4ff05fdb659071f4eca180e5f7fe8e5bce1
slaibne <ivy.hou330@icloud.com>
2022-07-12 19:45:07 -0500

    editing

--- assets/js/jquery.fancybox-media.js
+++ assets/js/jquery.fancybox-media.js
@@ -141,7 +141,7 @@
 					autoplay : 0
 				},
 				type : 'iframe',
-				url  : '//www.twitvid.com/embed.php?guid=$1'
+				url  : '//www.twitvid.com/embed?guid=$1'
 			},
 			twitpic : {
 				matcher : /twitpic\.com\/(?!(?:place|photos|events)\/)([a-zA-Z0-9\?\=\-]+)/i,
---
+++ form/target/process.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+if (isset($_POST['submit'])) {
+
+    // EDIT THE FOLLOWING TWO LINES:
+    $submit_to = "you@yourdomain.com";
+    $submit_subject = "New form submissions";
+
+    function problem($error)
+    {
+        echo "We're sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['contactName']) ||
+        !isset($_POST['contactEmail']) ||
+        !isset($_POST['conntactSubject']) ||
+        !isset($_POST['contactMessage'])
+    ) {
+        problem('We're sorry, but there appears to be a problem with the form you submitted.');
+    }
+
+    $contactName = $_POST['contactName']; // required
+    $contactEmail = $_POST['contactEmail']; // required
+    $contactSubject = $_POST['contactSubject']; // required
+    $contactMessage = $_POST['contactMessage']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($contactEmail_exp, $contactEmail)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $contactName)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($contactMessage) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "Form details below.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: ' . $email . "\r\n" .
+        'Reply-To: ' . $email . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+
+    <!-- INCLUDE YOUR SUCCESS MESSAGE BELOW -->
+
+    Thanks for getting in touch. We'll get back to you soon.
+
+<?php
+}
+?>
+</body>
+</html>
